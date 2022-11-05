8154fa5fb497cfa7fab2e357266cdf8f3f3c94e8
mael314 <mael.nivel@gmail.com>
2022-11-05 14:48:18 +0100

    [TEST] test auto deploy

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SensoNord
+          SensoNord - Test task
         </p>
         <a
           className="App-link"
