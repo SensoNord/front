df8b7ef59533b204486b6dccbfffacb51dccbdfd
mael314 <mael.nivel@gmail.com>
2022-11-05 18:02:45 +0100

    [GIT] final test merge

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SensoNord - Test task
+          SensoNord - Test merge
         </p>
         <a
           className="App-link"
