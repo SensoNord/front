1c308e72b7e8a5a7caa33f5a4736109a4ddb9986
mael <mael.nivel@gmail.com>
2022-12-15 21:23:29 +0100

    new featue

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          SensoNord - Test merge
+          SensoNord - Test new feature
         </p>
         <a
           className="App-link"
