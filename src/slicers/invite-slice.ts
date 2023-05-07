938f5c5192c7db4185c5fb89709542218cc220b7
Maël NIVEL <mael.nivel@gmail.com>
2023-05-07 21:00:09 +0200

    invite slice

--- src/slicers/invite-slice.ts
+++ src/slicers/invite-slice.ts
@@ -1,7 +1,59 @@
+import { createAsyncThunk, createSlice } from '@reduxjs/toolkit';
+import { directus } from '../libraries/directus';
 import { ErrorType } from '../types/Request/ErrorType';
 import { StatusEnum } from '../types/Request/StatusEnum';
+import { InvitationType } from '../types/Users/InvitationType';
 
 interface InviteState {
-  status: StatusEnum;
-  error: ErrorType;
+    invitationList: Array<InvitationType>;
+    status: StatusEnum;
+    error: ErrorType;
 }
+
+const initialState: InviteState = {
+    invitationList: [] as Array<InvitationType>,
+    status: StatusEnum.IDLE,
+    error: {} as ErrorType,
+};
+
+export const fetchInvite = createAsyncThunk(
+    'invite/fetchInvite',
+    async (invitation: InvitationType, { rejectWithValue }) => {
+        try {
+            await directus.users.invites.send(
+                invitation.email,
+                invitation.roleId,
+            );
+            return invitation;
+        } catch (error: any) {
+            return rejectWithValue({
+                error: error.message,
+                status: error.response.status,
+            });
+        }
+    },
+);
+
+const inviteSlice = createSlice({
+    name: 'invitation',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(fetchInvite.pending, state => {
+                state.status = StatusEnum.LOADING;
+                state.error = {} as ErrorType;
+            })
+            .addCase(fetchInvite.fulfilled, (state, action) => {
+                state.status = StatusEnum.SUCCEEDED;
+                state.invitationList.push(action.payload);
+                state.error = {} as ErrorType;
+            })
+            .addCase(fetchInvite.rejected, (state, action) => {
+                state.status = StatusEnum.FAILED;
+                state.error = action.payload as any;
+            });
+    },
+});
+
+export default inviteSlice.reducer;
--- src/slicers/role-slice.ts
+++ src/slicers/role-slice.ts
@@ -5,56 +5,56 @@ import { StatusEnum } from '../types/Request/StatusEnum';
 import { ErrorType } from '../types/Request/ErrorType';
 
 interface RoleState {
-  roles: Array<RoleType>;
-  status: StatusEnum;
-  error: ErrorType;
+    roles: Array<RoleType>;
+    status: StatusEnum;
+    error: ErrorType;
 }
 
 const initialState: RoleState = {
-  roles: [],
-  status: StatusEnum.IDLE,
-  error: {} as ErrorType,
+    roles: [] as Array<RoleType>,
+    status: StatusEnum.IDLE,
+    error: {} as ErrorType,
 };
 
 export const fetchRoles = createAsyncThunk(
-  'roles/fetchRoles',
-  async (_, { rejectWithValue }) => {
-    try {
-      const response = await directus.roles.readByQuery({
-        limit: -1,
-      });
-      return response.data as Array<RoleType>;
-    } catch (error: any) {
-      return rejectWithValue({
-        error: error.message,
-        status: error.response.status,
-      });
-    }
-  },
+    'roles/fetchRoles',
+    async (_, { rejectWithValue }) => {
+        try {
+            const response = await directus.roles.readByQuery({
+                limit: -1,
+            });
+            return response.data as Array<RoleType>;
+        } catch (error: any) {
+            return rejectWithValue({
+                error: error.message,
+                status: error.response.status,
+            });
+        }
+    },
 );
 
 const roleSlice = createSlice({
-  name: 'roles',
-  initialState,
-  reducers: {},
-  extraReducers: builder => {
-    builder
-      .addCase(fetchRoles.pending, state => {
-        state.status = StatusEnum.LOADING;
-        state.roles = [];
-        state.error = {} as ErrorType;
-      })
-      .addCase(fetchRoles.fulfilled, (state, action) => {
-        state.status = StatusEnum.SUCCEEDED;
-        state.roles = action.payload;
-        state.error = {} as ErrorType;
-      })
-      .addCase(fetchRoles.rejected, (state, action) => {
-        state.status = StatusEnum.FAILED;
-        state.roles = [];
-        state.error = action.payload as any;
-      });
-  },
+    name: 'roles',
+    initialState,
+    reducers: {},
+    extraReducers: builder => {
+        builder
+            .addCase(fetchRoles.pending, state => {
+                state.status = StatusEnum.LOADING;
+                state.roles = [] as Array<RoleType>;
+                state.error = {} as ErrorType;
+            })
+            .addCase(fetchRoles.fulfilled, (state, action) => {
+                state.status = StatusEnum.SUCCEEDED;
+                state.roles = action.payload;
+                state.error = {} as ErrorType;
+            })
+            .addCase(fetchRoles.rejected, (state, action) => {
+                state.status = StatusEnum.FAILED;
+                state.roles = [] as Array<RoleType>;
+                state.error = action.payload as any;
+            });
+    },
 });
 
 export default roleSlice.reducer;
