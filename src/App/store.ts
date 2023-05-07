d5398570a94dfdbba89c0ab0379df391597f33b3
Maël NIVEL <mael.nivel@gmail.com>
2023-05-07 16:51:28 +0200

    add feature to get role list

--- src/App/store.ts
+++ src/App/store.ts
@@ -1,13 +1,14 @@
-import { configureStore, ThunkDispatch } from "@reduxjs/toolkit";
+import { AnyAction, configureStore, ThunkDispatch } from "@reduxjs/toolkit";
 import authReducer from "../slicers/auth-slice";
 import subjectReducer from "../slicers/subject-slice";
-import { AnyAction } from 'redux';
+import roleReducer from "../slicers/role-slice";
 import { errorMiddleware } from "./middleware";
 
 export const store = configureStore({
     reducer: {
         auth: authReducer,
-        subject: subjectReducer
+        subject: subjectReducer,
+        role: roleReducer
     },
     middleware: (getDefaultMiddleware) => [
         ...getDefaultMiddleware(),
