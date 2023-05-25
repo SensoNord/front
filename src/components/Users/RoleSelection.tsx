7da15bc3904c02974f36a43ed90f4934451bd4fd
Corentin <corentin.delhonb@protonmail.com>
2023-05-25 23:25:45 +0200

    Delete Administrator for role selection

--- src/components/Users/RoleSelection.tsx
+++ src/components/Users/RoleSelection.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import {useEffect, useState} from 'react';
 import { useAppDispatch, useAppSelector } from '../../App/hooks';
 import { RoleType } from '@directus/sdk';
 import { fetchRoles } from '../../slicers/user/role-slice';
@@ -15,20 +15,25 @@ export default function RoleSelection(props: RoleSelectionProps) {
     const { selectedRole, setSelectedRole, setIsRoleValid, classNameSelection } = props;
     const dispatch = useAppDispatch();
     const { roles } = useAppSelector(state => state.role);
+    const [cleanedRoles, setCleanedRoles] = useState<RoleType[]>([]);
 
     useEffect(() => {
         dispatch(fetchRoles());
     }, [dispatch]);
+    
+    useEffect(() => {
+        setCleanedRoles(roles.filter((role: RoleType) => !role.admin_access));
+    }, [roles])
 
     const handleChange = (event: React.ChangeEvent<HTMLSelectElement>) => {
-        const role = roles.find((role: RoleType) => role.name === event.target.value);
+        const role = cleanedRoles.find((role: RoleType) => role.name === event.target.value);
         setSelectedRole(role);
         role ? setIsRoleValid(true) : setIsRoleValid(false);
     };
 
     return (
         <SelectField
-            options={roles.map((role: RoleType) => ({
+            options={cleanedRoles.map((role: RoleType) => ({
                 value: role.name,
                 label: role.name,
             }))}
